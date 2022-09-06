6c5f1cc23b81a8e78c7dca058041b969a4e52084
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2022-09-05 23:08:38 -0300

    requisito 1

---
+++ src/database/migrations/20220905190026-PostCategories.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PostCategories', {
+      postId: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        // references: {
+        //   model: 'BlogPosts',
+        //   key: id
+        // }
+      },
+      categoryId: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        // references: {
+        //   model: 'Categories',
+        //   key: id
+        // }
+      },
+     })
+    },
+  
+    down: async (queryInterface, _Sequelize) => await queryInterface.dropTable('PostCategories')
+};
